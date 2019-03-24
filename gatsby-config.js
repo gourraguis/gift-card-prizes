fa4fe4aeb18ab3f2306c41493063adb29cf4d3b8
gourraguis <gourragui.success@gmail.com>
2019-03-24 01:27:34 +0100

    update siteMetadata title & description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Gift Card Prizes Generator Pro: Get Free Codes & Vouchers Online',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Gift Card Prizes is the best place to get Gift Card Codes for Free. It is time to generate your vouchers and get anything for free!',
   },
   plugins: [
     {
